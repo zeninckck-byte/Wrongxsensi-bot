e37c1dc9b52b23bc6339fdd5ff69ef5790163544
zeninckck-byte <zeninckck@gmail.com>
2026-07-22 15:22:21 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,4 +1,7 @@
 import secrets
+from datetime import datetime, timedelta
 
-def generate_key():
-    return "WRONG-" + secrets.token_hex(4).upper()
+def generate_key(days):
+    key = "WRONG-" + secrets.token_hex(4).upper()
+    expiry = datetime.now() + timedelta(days=days)
+    return key, expiry.strftime("%Y-%m-%d %H:%M:%S")
