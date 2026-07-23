6289ac984aa85908e4a47457ec713d4389d4b3ac
zeninckck-byte <zeninckck@gmail.com>
2026-07-23 12:39:37 +0530

    Create api.py

---
+++ api.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "API Running"
+
+@app.route("/ip")
+def ip():
+    return {
+        "ip": request.remote_addr
+    }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
